939fc4965bdb499f157b9fcc89108388d87c6324
Bobby Mushkot <bobby.mushkot@gmail.com>
2020-09-04 17:20:57 -0700

    Now gets job descriptions and added functions

--- boardReader.py
+++ boardReader.py
@@ -4,70 +4,44 @@ from selenium import webdriver
 import requests
 import os
 from selenium.webdriver.firefox.options import Options
-
+import time
 #----------------------------------#
 #         Selenium functions
-def openFireFox(webBrowser):
-    glassdoor = 'https://www.glassdoor.com/index.htm'
-    webBrowser.get(glassdoor)
 
-def jobTitle(webBrowser, title):
-    jobSearch = webBrowser.find_element_by_id("sc.keyword")
-    jobSearch.click()
-    jobSearch.clear()
-    jobSearch.send_keys(title)
+def getInfo(results):
+    companies = results.find_elements_by_class_name('company')
+    titles = results.find_elements_by_class_name("title")
 
-def jobLocation(webBrowser, location):
-    locationSearch = webBrowser.find_element_by_id("sc.location")
-    locationSearch.click()
-    locationSearch.clear()
-    locationSearch.send_keys(location)
+    return companies, titles
 
-def getCompany(webBrowser, num):
-    try:
-        return webBrowser.find_element_by_css_selector(f'li.jl:nth-child({num}) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1)').text
-    except:
-        return 'None'
-
-def getJobTitle(webBrowser, num):
-    try:
-        return webBrowser.find_element_by_css_selector(f'li.jl:nth-child({num}) > div:nth-child(2) > a:nth-child(2)').text
-    except:
-        return 'None'
-
-def getJobLocation(webBrowser, num):
-    try:
-        return webBrowser.find_element_by_css_selector(f"li.jl:nth-child({num}) > div:nth-child(2) > div:nth-child(3) > span:nth-child(1)").text
-    except:
-        return 'None'
-
-def getJobSalary(webBrowser, num):
-    try:
-        return webBrowser.find_element_by_css_selector(f"li.jl:nth-child({num}) > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").text
-    except:
-        return 'None'
-
-def nextPage(webBrowser):
-    webBrowser.find_element_by_css_selector(".next").click()
+def printPageInfo(driver, companies, titles):
+    
+    for i in range(len(companies)):
+        print("\n----------- JOB " + str(i+1) + " DESCRIPTION--------------\n")
+        print(companies[i].text)
+        print(titles[i].text.split("\n")[0] + "\n")
+        companies[i].click()
+        driver.switch_to.frame('vjs-container-iframe')
+        time.sleep(3)
+        jobDes = driver.find_element_by_xpath('//*[@id="jobDescriptionText"]')
+        print(jobDes.text)
+        driver.switch_to.default_content()
 
 def main():
     options = Options()
     options.headless = True    
     webBrowser = webdriver.Firefox(options=options)
-    URL = "https://www.indeed.com/q-software-engineer-l-Valencia,-CA-jobs.html"
+    URL = "https://www.indeed.com/jobs?q=software+engineer&l=San+Jose%2C+CA"
     webBrowser.get(URL)
+    
     resultsCol = webBrowser.find_element_by_id('resultsCol')
-    
-    companies = resultsCol.find_elements_by_class_name('company')
-    titles = resultsCol.find_elements_by_class_name("title")
+    companies, titles = getInfo(resultsCol)
 
-    for i in range(len(companies)):
-        print("----------")
-        print(companies[i].text)
-        print(titles[i].text.split("\n")[0])
-    
+    printPageInfo(webBrowser, companies, titles)
+      
     webBrowser.quit()
 
+    
 if __name__ == '__main__':
     main()
 
