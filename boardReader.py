d8ffa54d05cdb7ed3e2f36cf013a5fcc6291d5bd
Bobby Mushkot <bobby.mushkot@gmail.com>
2020-09-11 13:36:20 -0700

    Gets counts of words in languages file

--- boardReader.py
+++ boardReader.py
@@ -5,34 +5,64 @@ import requests
 import os
 from selenium.webdriver.firefox.options import Options
 import time
+from collections import Counter
+import re
 #----------------------------------#
 #         Selenium functions
 
-def getInfo(results):
+# Pull the info from the browser
+# Returns a list of companies and titles elements
+# as well as the job description text
+def getInfo(driver, results):
     companies = results.find_elements_by_class_name('company')
     titles = results.find_elements_by_class_name("title")
-
-    return companies, titles
-
-def printPageInfo(driver, companies, titles):   
+    descriptions = []
+    print("Getting job descriptions")
     for i in range(len(companies)):
         try:
-            print("\n----------- JOB " + str(i+1) + " DESCRIPTION--------------\n")
-            print(companies[i].text)
-            print(titles[i].text.split("\n")[0] + "\n")
             titles[i].click()
             time.sleep(.5)
             driver.switch_to.frame('vjs-container-iframe')
             time.sleep(1.5)
             jobDes = driver.find_element_by_xpath('//*[@id="jobDescriptionText"]')
-            print(jobDes.text)
+            descriptions.append(jobDes.text.lower())
             driver.switch_to.default_content()
         except Exception as e:
             driver.switch_to.default_content()
-            print("******* Error with job " + str(i))
+            print("******* Error getting job " + str(i))
             print(e)
             continue
 
+    return companies, titles, descriptions
+
+# Print the company, job title, and the job description for a single page. 
+def printPageInfo(companies, titles, descriptions):
+    for i in range(len(companies)):
+        try:
+            print("\n----------- JOB " + str(i+1) + " DESCRIPTION--------------\n")
+            print(companies[i].text)
+            print(titles[i].text.split("\n")[0] + "\n")
+            print(descriptions[i])
+        except Exception as e:
+            print("******* Error printing job " + str(i))
+            print(e)
+            continue
+
+def createLanguageCounter(fileLocation):
+    wordCounter = Counter()
+    with open(fileLocation, 'r') as languageFile:
+        for line in languageFile:
+            wordCounter[line.lower().rstrip()] = 0
+    languageFile.close()
+    return wordCounter
+
+def getWordCount(wordCounter, descriptions):
+    for job in descriptions:
+        job = re.sub(r"[,.;@#?!&$/]+\ *", " ", job)
+        for word in job.split():
+            if word in wordCounter:
+                wordCounter[word] +=1
+    return wordCounter
 def main():
     options = Options()
     options.headless = True    
@@ -41,10 +71,12 @@ def main():
     webBrowser.get(URL)
     
     resultsCol = webBrowser.find_element_by_id('resultsCol')
-    companies, titles = getInfo(resultsCol)
-
-    printPageInfo(webBrowser, companies, titles)
-      
+    companies, titles, descriptions = getInfo(webBrowser, resultsCol)
+    #  maybe pages = [(companies, titles, descriptions)]
+    printPageInfo(companies, titles, descriptions)
+    wordCounter = createLanguageCounter('languages.txt')
+    wordCounter = getWordCount(counter, descriptions)
+    print(wordCounter)
     webBrowser.quit()
 
     
